03733fa8951295ee41a916cc5236752ccc6f6056
Astalsi401 <misting401@gmail.com>
2025-06-28 01:00:23 +0800

    add: test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,11 +2,10 @@
 
 import { Button } from "@headlessui/react";
 import { openModal } from "@/components/modal/modal-slice";
-import { useAppDispatch, useAppSelector } from "@/hooks";
+import { useAppDispatch } from "@/hooks";
 
 export default function TestPage() {
   const dispatch = useAppDispatch();
-  const open = useAppSelector((state) => state.modal.open);
   return (
     <div>
       <Button onClick={() => dispatch(openModal("test"))}>Open modal</Button>
