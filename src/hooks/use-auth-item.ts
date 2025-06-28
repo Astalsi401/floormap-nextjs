98fa067beeaf73d3701e7f2bb30ee633f5797f2d
Astalsi401 <misting401@gmail.com>
2025-06-29 02:33:02 +0800

    update: simplified useAuthItem hook

--- src/hooks/use-auth-item.ts
+++ src/hooks/use-auth-item.ts
@@ -1,34 +1,23 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import { Session } from "next-auth";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Routes } from "@/config/routes";
-import type { MenuItemType } from "@/types/menu-items";
+import type { MenuItemType } from "@/types";
 
 export const useAuthItem = (): {
   login: boolean;
   user?: Session["user"];
   authItem: MenuItemType;
 } => {
-  const searchParams = useSearchParams();
   const { data: session } = useSession();
-  const signoutItem = {
-    key: Routes.auth.signOut,
-    label: "Sign out",
-    onClick: () => signOut(),
-  };
-  const signinItem = {
-    key: Routes.auth.signIn,
-    label: "Sign in",
-    onClick: () =>
-      signIn("github", {
-        callbackUrl: searchParams.get("callbackUrl") || "/",
-      }),
-  };
   return {
     login: !!session,
     user: session?.user,
-    authItem: session ? signoutItem : signinItem,
+    authItem: {
+      key: "authItem",
+      ...(session
+        ? { label: "Sign out", onClick: () => signOut() }
+        : { label: "Sign in", onClick: () => signIn() }),
+    },
   };
 };
