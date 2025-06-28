cca4cdd56c54ddedaa736e24456afaee4bc76d5d
Astalsi401 <misting401@gmail.com>
2025-06-28 12:07:38 +0800

    add: forwardRef to custom button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React from "react";
+import { forwardRef } from "react";
 import { Button as HeadlessButton } from "@headlessui/react";
 
 type ButtonProps<ComponentType extends React.ElementType> = {
@@ -7,33 +7,40 @@ type ButtonProps<ComponentType extends React.ElementType> = {
   component?: ComponentType;
 } & React.ComponentPropsWithoutRef<ComponentType>;
 
-export const Button = <
-  ComponentType extends React.ElementType = typeof HeadlessButton
->({
-  component,
-  loading = false,
-  className,
-  children,
-  onClick,
-  ...rest
-}: ButtonProps<ComponentType>) => {
-  const Component = component || HeadlessButton;
+const Button = forwardRef(
+  <ComponentType extends React.ElementType = typeof HeadlessButton>(
+    {
+      component,
+      loading = false,
+      className,
+      children,
+      onClick,
+      ...rest
+    }: ButtonProps<ComponentType>,
+    ref: React.ForwardedRef<ComponentType>
+  ) => {
+    const Component = component || HeadlessButton;
 
-  return (
-    <Component
-      className={clsx(
-        className,
-        "relative overflow-hidden",
-        loading &&
-          "before:content-[''] before:block before:absolute before:inset-0 before:animate-btn-loading",
-        loading &&
-          "before:bg-linear-90 before:from-transparent before:via-fp-lv3/10 before:to-transparent",
-        loading ? "cursor-wait" : "cursor-pointer"
-      )}
-      onClick={onClick ? (loading ? undefined : onClick) : undefined}
-      {...rest}
-    >
-      {children}
-    </Component>
-  );
-};
+    return (
+      <Component
+        ref={ref}
+        className={clsx(
+          className,
+          "relative overflow-hidden",
+          loading &&
+            "before:content-[''] before:block before:absolute before:inset-0 before:animate-btn-loading",
+          loading &&
+            "before:bg-linear-90 before:from-transparent before:via-fp-lv3/10 before:to-transparent",
+          loading ? "cursor-wait" : "cursor-pointer"
+        )}
+        onClick={onClick ? (loading ? undefined : onClick) : undefined}
+        {...rest}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button };
