05fcf36d0cdb8abd5d644024e1bd8021e044f67f
Astalsi401 <misting401@gmail.com>
2025-06-28 14:15:38 +0800

    test: vercel env

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  env: {
-    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
-    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
-    NEXT_PUBLIC_REST_API_ENDPOINT: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
-  },
 };
 
 export default nextConfig;
