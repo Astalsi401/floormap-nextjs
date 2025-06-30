0ca864c7303f3edc5410de012a3e0d913d4e511b
Astalsi401 <misting401@gmail.com>
2025-06-30 22:03:52 +0800

    fix: build error

--- src/app/(public)/floormap/[exhibition]/[year]/layout.tsx
+++ src/app/(public)/floormap/[exhibition]/[year]/layout.tsx
@@ -4,17 +4,14 @@ import { Spinner } from "@ui/loading/spinner";
 
 export default async function FloorMapLayout({
   children,
-  params,
 }: {
   children: Readonly<React.ReactNode>;
-  params: Promise<{ exhibition: string; year: string }>;
 }) {
-  const { exhibition, year } = await params;
   return (
     <div data-floormap className="-mt-16.25 h-screen">
       <div className="pt-16.25 ps-80 h-full">
         <Suspense fallback={<Spinner className="size-10" />}>
-          <Sidebar exhibition={exhibition} year={year} />
+          <Sidebar />
         </Suspense>
         {children}
       </div>
